9c7d344ddb899536e463bb68ab5c74c0844c6ae9
Bebe Jane <bjornthief@gmail.com>
2023-10-24 15:49:20 +0200

    ja

--- lib/shopify/api/handlers/sync/shopify-sync.ts
+++ lib/shopify/api/handlers/sync/shopify-sync.ts
@@ -1,22 +1,17 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-//import { syncObjects, deleteObject } from '../../../sync'
+import { syncObjects, deleteObject } from '../../../sync'
 
-export default async function shopifyToDatoCMSSync(req: NextApiRequest, res: NextApiResponse) {
-  return res.status(200).json({ success: true })
-}
-/*
 export default async function shopifyToDatoCMSSync(req: NextApiRequest, res: NextApiResponse) {
 
   try {
 
     const event = (req.headers['x-shopify-topic'] as string)?.split('/')?.[1]
-    console.log('sync event', event)
+
     if (!event)
       return res.status(400).json({ success: false, error: 'event type not found in headers' })
 
     const data = req.body
 
-    console.log('sync data', data)
     if (!data)
       return res.status(400).json({ success: false, error: 'item not found' })
 
@@ -30,9 +25,7 @@ export default async function shopifyToDatoCMSSync(req: NextApiRequest, res: Nex
     return res.status(200).json({ success: true, event })
 
   } catch (error) {
-    console.log('sync error', error)
-    //console.log(JSON.stringify(error, null, 2))
+    console.log(JSON.stringify(error, null, 2))
     return res.status(500).json({ success: false, error })
   }
 }
-*/
