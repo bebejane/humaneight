2208f1585aec22c4b2d569cbff8253f596d1ac7d
Bebe Jane <bjornthief@gmail.com>
2023-11-24 13:39:09 +0100

    t

--- app/products/[product]/page.tsx
+++ app/products/[product]/page.tsx
@@ -4,6 +4,8 @@ import { apiQuery } from 'next-dato-utils';
 
 
 import cn from 'classnames';
+import shopifyQuery from '@shopify/shopify-query';
+import { ShopifyProductDocument } from '@shopify/graphql';
 
 export async function generateStaticParams() {
   const { allProducts } = await apiQuery<AllProductsQuery, AllProductsQueryVariables>(AllProductsDocument, {
@@ -20,13 +22,13 @@ export default async function Product({ params }: { params: { product: string } 
 
   //const { product, draftUrl } = await apiQuery<ProductQuery, ProductQueryVariables>(ProductDocument, { variables: { slug: params.product } });
   //const { allProductColors } = await apiQuery<AllProductColorsQuery, AllProductColorsQueryVariables>(AllProductColorsDocument, { all: true });
-  //const { product: shopifyProduct } = await shopifyQuery<ShopifyProductQuery, ShopifyProductQueryVariables>(ShopifyProductDocument, { variables: { handle: params.product } });
+  const { product: shopifyProduct } = await shopifyQuery<ShopifyProductQuery, ShopifyProductQueryVariables>(ShopifyProductDocument, { variables: { handle: params.product } });
   //const variant = shopifyProduct?.variants?.edges[0]?.node as ProductVariant
 
 
   //if (!shopifyProduct)
   //return <div>Shopify product not found for slug {params.product}</div>
 
-  return null
+  return <>{shopifyProduct?.handle}</>
 
 }
