82e649efd231ab19f0b9aab317a22daaa093d998
Bebe Jane <bjornthief@gmail.com>
2023-11-24 13:54:47 +0100

    a

--- app/products/[product]/page.tsx
+++ app/products/[product]/page.tsx
@@ -1,9 +1,9 @@
 'use server'
 
 import s from './page.module.scss'
-//import { notFound } from 'next/navigation';
+import { notFound } from 'next/navigation';
 import { AllProductColorsDocument, AllProductsDocument, ProductDocument } from '@graphql';
-import { apiQuery } from 'next-dato-utils';
+import { DraftMode, apiQuery } from 'next-dato-utils';
 import shopifyQuery from '@shopify/shopify-query';
 import { ShopifyProductDocument } from '@shopify/graphql';
 import StructuredContent from '@components/layout/StructuredContent';
@@ -26,20 +26,37 @@ export async function generateStaticParams() {
 
 export default async function Product({ params }: { params: { product: string } }) {
 
-  //const { product, draftUrl } = await apiQuery<ProductQuery, ProductQueryVariables>(ProductDocument, { variables: { slug: params.product } });
+  const { product, draftUrl } = await apiQuery<ProductQuery, ProductQueryVariables>(ProductDocument, { revalidate: 20, variables: { slug: params.product } });
   const { allProductColors } = await apiQuery<AllProductColorsQuery, AllProductColorsQueryVariables>(AllProductColorsDocument, { all: true });
   const { product: shopifyProduct } = await shopifyQuery<ShopifyProductQuery, ShopifyProductQueryVariables>(ShopifyProductDocument, { variables: { handle: params.product } });
   const variant = shopifyProduct?.variants?.edges[0]?.node as ProductVariant
 
-  //if (!product)
-  //return notFound();
+  if (!product)
+    return notFound();
 
   if (!shopifyProduct)
     return <div>Shopify product not found for slug {params.product}</div>
 
   return (
     <>
-      hej
+      <section className={cn(s.product, "grid")}>
+        <div className={s.details}>
+          <p className="small">
+            <Link href="/shop">Shop</Link>
+            &nbsp;|&nbsp;
+            <Link href={`/shop/${product.collection.slug}`}>{product.collection.title}</Link>
+          </p>
+          <header>
+            <h1 className="body">{product.title}</h1>
+            <div className={s.price}><p><Price money={variant.price} /></p></div>
+          </header>
+          <StructuredContent id={product.id} content={product.shortSummary} />
+          <span>Read more</span>
+          <VariantsForm allProductColors={allProductColors} shopifyProduct={shopifyProduct} />
+        </div>
+        <ProductPresentation product={product} shopifyProduct={shopifyProduct} />
+      </section>
+      <DraftMode url={draftUrl} tag={product.id} />
     </>
   )
 }
