a90b1fe6101ddc44de2d938d221f97f39a9bc256
Bebe Jane <bjornthief@gmail.com>
2023-11-28 10:42:57 +0100

    blocks start

---
+++ components/blocks/start/index.tsx
@@ -0,0 +1,4 @@
+export { default as StartProductShortcutBlock } from './StartProductShortcutBlock';
+export { default as StartProductBlock } from './StartProductBlock';
+export { default as StartFullscreenBlock } from './StartFullscreenBlock';
+export { default as StartEditorialBlock } from './StartEditorialBlock';
