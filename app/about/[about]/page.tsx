1e24b15bfa7bdc99131dbbc321579ede56ed65a8
Bebe Jane <bjornthief@gmail.com>
2023-11-28 10:40:37 +0100

    about page

--- app/about/[about]/page.tsx
+++ app/about/[about]/page.tsx
@@ -2,6 +2,8 @@
 import { AboutDocument, AllAboutsDocument } from '@graphql';
 import { apiQuery, DraftMode } from 'next-dato-utils';
 import { notFound } from 'next/navigation';
+import { Block } from 'next-dato-utils'
+import AboutTwoColumnBlock from "./components/AboutTwoColumnBlock"
 
 export async function generateStaticParams() {
   const { allAbouts } = await apiQuery<AllAboutsQuery, AllAboutsQueryVariables>(AllAboutsDocument, {
@@ -24,6 +26,9 @@ export default async function About({ params }: { params: { about: string } }) {
   return (
     <>
       <h1>{title}</h1>
+      {about.sections.map((section, i) =>
+        <Block key={i} data={section} components={[AboutTwoColumnBlock]} />
+      )}
       <DraftMode url={draftUrl} tag={about.id} />
     </>
   )
