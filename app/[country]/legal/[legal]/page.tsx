d0ccd9b510f7c77c68a1f8399f238c0cbc92c27c
Bebe Jane <bjornthief@gmail.com>
2023-11-29 12:04:38 +0100

    legal localization

---
+++ app/[country]/legal/[legal]/page.tsx
@@ -0,0 +1,7 @@
+import page from '@app/legal/[legal]/page';
+import { CountryParams } from '@app/[country]/layout';
+export { generateStaticParams } from '@app/legal/[legal]/page';
+
+export type CountryAboutParams = CountryParams & { params: { legal: string; } };
+
+export default async (params: CountryAboutParams) => page(params);
