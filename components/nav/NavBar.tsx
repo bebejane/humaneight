b7adeb628d5d988723eeb9454ea550999ec47db7
Bebe Jane <bjornthief@gmail.com>
2023-11-29 11:56:45 +0100

    new menu

--- components/nav/NavBar.tsx
+++ components/nav/NavBar.tsx
@@ -33,16 +33,23 @@ export default function NavBar({ menu, localization }: Props) {
     <>
       <nav className={s.navbar}>
         <menu>
-          <button className="nav nav-hover" onClick={() => setShowMenu(!showMenu)}>Menu</button>
-          <h1 className="nav"><Link href="/">Humaneight</Link></h1>
-          <Cart localization={localization} />
+          <button
+            className={cn(s.menu, showMenu && s.active, 'nav', 'nav-hover')}
+            onClick={() => setShowMenu(!showMenu)}
+          >Menu</button>
+          <h1 className="nav">
+            <Link href="/">Humaneight</Link>
+          </h1>
+          {showMenu ?
+            <button className={cn(s.close, "nav", "nav-hover")} onClick={() => setShowMenu(false)}>
+              Close
+            </button>
+            :
+            <Cart localization={localization} />
+          }
         </menu>
       </nav>
       <nav className={cn(s.desktop, showMenu && s.show)}>
-        <button className={cn(s.menu, 'nav', 'nav-hover')} onClick={() => setShowMenu(!showMenu)}>
-          Menu
-        </button>
-        <button className={cn(s.close, "nav", "nav-hover")} onClick={() => setShowMenu(false)}>Close</button>
         <ul>
           {menu.slice(0, 3).map(({ id, title, sub, slug }) => (
             <li key={id}>
