f2e7b349f4d3d5a2daacaffb512498101af2285b
Bebe Jane <bjornthief@gmail.com>
2023-12-01 15:06:03 +0100

    fix

--- components/nav/Logo.tsx
+++ components/nav/Logo.tsx
@@ -10,12 +10,12 @@ import { CSSProperties, useEffect, useState } from 'react';
 export default function Logo() {
 
   const pathname = usePathname()
-  const [style, setStyle] = useState<CSSProperties | undefined>({ opacity: 0 })
+  const [style, setStyle] = useState<CSSProperties | undefined>({ opacity: 1 })
   const { scrolledPosition, documentHeight, viewportHeight } = useScrollInfo()
 
   useEffect(() => {
     const ratio = Math.min((scrolledPosition / viewportHeight) * 2, 1)
-    const maxLogoSize = 'calc(var(--logo-size) * var(--logo-size-max-ratio))'
+    const maxLogoSize = 'var(--logo-size-intro)'
     const fontSize = `calc(calc(${maxLogoSize} * ${1 - ratio}) + calc(var(--logo-size) * ${ratio})`
     const marginTop = `calc(calc(calc(50vh - calc(${maxLogoSize} / 2 )) - calc(var(--navbar-height) / 2 )) * ${1 - ratio})`
 
@@ -25,8 +25,13 @@ export default function Logo() {
   }, [pathname, scrolledPosition])
 
   return (
-    <h1 className={cn('nav', s.logo)} style={style}>
-      <Link href="/">Humaneight</Link>
-    </h1>
+    <>
+      <h1 className={cn('nav', s.logo)} style={style}>
+        <Link href="/">Humaneight</Link>
+      </h1>
+      <div className={cn('grid', s.tagline)}>
+        <h2><span>For</span> <span>a</span> <span>neurodiverse</span> <span>world.</span></h2>
+      </div>
+    </>
   )
 }
