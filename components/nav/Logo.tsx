b3950c8da5ed180dda6590a7c8318890cb5aa264
Bebe Jane <bjornthief@gmail.com>
2023-12-01 15:32:45 +0100

    fix

--- components/nav/Logo.tsx
+++ components/nav/Logo.tsx
@@ -3,6 +3,7 @@
 import s from './Logo.module.scss';
 import cn from 'classnames';
 import { useScrollInfo } from 'next-dato-utils';
+import { INSTRUMENTATION_HOOK_FILENAME } from 'next/dist/lib/constants';
 import Link from "next/link";
 import { usePathname } from 'next/navigation';
 import { CSSProperties, useEffect, useState } from 'react';
@@ -12,8 +13,9 @@ const tagline = ['For', 'a', 'neurodiverse', 'world.']
 export default function Logo() {
 
   const pathname = usePathname()
+  const isHome = pathname === '/'
   const [style, setStyle] = useState<CSSProperties | undefined>({})
-  const { scrolledPosition, documentHeight, viewportHeight } = useScrollInfo()
+  const { scrolledPosition, viewportHeight } = useScrollInfo()
   const taglineRatio = Math.min((scrolledPosition / viewportHeight) * 4, 1)
 
   useEffect(() => {
@@ -30,10 +32,10 @@ export default function Logo() {
 
   return (
     <>
-      <h1 className={cn('nav', s.logo, pathname === '/' && s.intro)} style={style}>
+      <h1 className={cn('nav', s.logo, isHome && s.intro)} style={style}>
         <Link href="/">Humaneight</Link>
       </h1>
-      <div className={cn('grid', s.tagline, pathname === '/' && s.intro)} style={{ opacity: 1 - taglineRatio }}>
+      <div className={cn('grid', s.tagline, isHome && s.intro)} style={{ opacity: 1 - taglineRatio }}>
         <h2>{tagline.map(word => <span>{word}</span>)}</h2>
       </div>
     </>
