c894d1290d29a39c95b9fa08c7cb1e09c5198349
Bebe Jane <bjornthief@gmail.com>
2023-12-01 16:08:38 +0100

    slutet

--- components/nav/Logo.tsx
+++ components/nav/Logo.tsx
@@ -13,30 +13,38 @@ const tagline = ['For', 'a', 'neurodiverse', 'world.']
 export default function Logo() {
 
   const pathname = usePathname()
-  const isHome = pathname === '/'
+  const [isHome, setIsHome] = useState(pathname === '/')
   const [style, setStyle] = useState<CSSProperties | undefined>({})
   const { scrolledPosition, viewportHeight } = useScrollInfo()
   const taglineRatio = Math.min((scrolledPosition / viewportHeight) * 4, 1)
+  const ratio = Math.min((scrolledPosition / viewportHeight) * 2, 1)
 
   useEffect(() => {
-    const ratio = Math.min((scrolledPosition / viewportHeight) * 2, 1)
-    const maxLogoSize = 'var(--logo-size-intro)'
-    const fontSize = `calc(calc(${maxLogoSize} * ${1 - ratio}) + calc(var(--logo-size) * ${ratio})`
-    const marginTop = `calc(calc(calc(50vh - calc(${maxLogoSize} / 2 )) - calc(var(--navbar-height) / 2 )) * ${1 - ratio})`
 
-    const style = pathname === '/' ? { fontSize, marginTop, opacity: 1 } : undefined
+    const maxLogoSize = 'var(--logo-size-intro)'
+    const fontSize = `calc(calc(calc(${maxLogoSize} - var(--logo-size)) * ${1 - ratio}) + var(--logo-size))`
+    const marginTop = `calc(calc(calc(50vh - calc(${maxLogoSize} / 2 )) - calc(var(--navbar-height) / 2 )) * ${1 - ratio})`
+    const style = { fontSize, marginTop, opacity: 1 }
     setStyle(style)
 
-  }, [pathname, scrolledPosition])
+  }, [scrolledPosition])
+
+  useEffect(() => {
+    setIsHome(pathname === '/')
+  }, [pathname])
 
   return (
     <>
-      <h1 className={cn('nav', s.logo, isHome && s.intro)} style={style}>
+      <h1
+        className={cn('nav', s.logo, isHome && s.intro, ratio > 0.9 && s.black)}
+        style={isHome ? style : undefined}
+      >
         <Link href="/">Humaneight</Link>
       </h1>
       <div className={cn('grid', s.tagline, isHome && s.intro)}>
-        <h2>{tagline.map(word =>
+        <h2>{tagline.map((word, i) =>
           <span
+            key={i}
             className={cn(taglineRatio > 0.1 && s.hide)}
             style={{ transitionDelay: `${Math.min(Math.random(), 0.5)}s` }} >
             {word}
