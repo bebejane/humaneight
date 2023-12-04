e55b5bd4f007a3ee0fcedb50a664bf8c581578a9
konst-teknik <information@konst-teknik.se>
2023-12-04 18:18:14 +0100

    Fix

--- app/products/[product]/components/ProductMeta.tsx
+++ app/products/[product]/components/ProductMeta.tsx
@@ -37,48 +37,55 @@ export default function ProductMeta({ product }: Props) {
   if (!metaSections) return null
 
   return (
-    <div className={cn(s.meta)}>
-      <div className={s.wrapper}>
-        {Object.keys(metaSections).map((k, idx) => {
-          const metaType = metaSections[k][0].metaType
-          return (
-            <React.Fragment key={idx}>
-              {metaType?.title &&
-                <a
-                  id={metaType.id}
-                  key={metaType.id}
-                  href={`#${metaType.id}`}
-                  className={s.metaType}
-                  onClick={() => setMetaSectionToggles({
-                    ...metaSectionToggles,
-                    [k]: {
-                      ...metaSectionToggles[k],
-                      show: !metaSectionToggles[k]?.show ? true : false
-                    }
-                  })}
+    <>
+      <div className={cn(s.meta)}>
+        <div className={s.wrapper}>
+          {Object.keys(metaSections).map((k, idx) => {
+            const metaType = metaSections[k][0].metaType
+            return (
+              <React.Fragment key={idx}>
+                {metaType?.title &&
+                  <a
+                    id={metaType.id}
+                    key={metaType.id}
+                    href={`#${metaType.id}`}
+                    className={s.metaType}
+                    onClick={() => setMetaSectionToggles({
+                      ...metaSectionToggles,
+                      [k]: {
+                        ...metaSectionToggles[k],
+                        show: !metaSectionToggles[k]?.show ? true : false
+                      }
+                    })}
+                  >
+                    <h3 className={s.type}>{metaType.title}</h3>
+                    <button className="symbol big">{!metaSectionToggles[k]?.show ? '+' : '-'}</button>
+                  </a>
+                }
+                <ul
+                  id={`list-${metaType.id}`}
+                  key={`list-${metaType.id}`}
+                  className={cn(metaSectionToggles[k]?.show ? s.show : s.hide)}
+                  style={{ maxHeight: metaSectionToggles[k]?.show ? `${metaSectionToggles[k].height}px` : '0px' }}
+                  ref={metaSectionToggles[k]?.show ? metaSectionRef : undefined}
                 >
-                  <h3 className={s.type}>{metaType.title}</h3>
-                  <button className="symbols">{!metaSectionToggles[k]?.show ? '+' : '-'}</button>
-                </a>
-              }
-              <ul
-                id={`list-${metaType.id}`}
-                key={`list-${metaType.id}`}
-                className={cn(metaSectionToggles[k]?.show ? s.show : s.hide)}
-                style={{ maxHeight: metaSectionToggles[k]?.show ? `${metaSectionToggles[k].height}px` : '0px' }}
-                ref={metaSectionToggles[k]?.show ? metaSectionRef : undefined}
-              >
-                {metaSections[k].map(({ id, title, text }) =>
-                  <li key={id} className="structured mid light">
-                    <StructuredContent content={text} />
-                  </li>
-                )}
-              </ul>
-            </React.Fragment>
-          )
-        })}
+                  {metaSections[k].map(({ id, title, text }) =>
+                    <li key={id} className="structured mid light">
+                      <StructuredContent content={text} />
+                    </li>
+                  )}
+                </ul>
+              </React.Fragment>
+            )
+          })}
+        </div>
+        <section className={s.inc}><p className="light mid">Always: Free returns — Free shipping over 1000 SEK – Taxes and duties included
+
+        </p></section>
+
       </div>
-    </div>
+    </>
+
 
   )
 }
