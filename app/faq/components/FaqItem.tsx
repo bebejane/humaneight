a25ee4500688bd626169eb4d2b19978d6ae503d6
Bebe Jane <bjornthief@gmail.com>
2023-12-05 09:57:34 +0100

    jajaja

--- app/faq/components/FaqItem.tsx
+++ app/faq/components/FaqItem.tsx
@@ -11,12 +11,18 @@ export type Props = {
 
 export default function FaqItem({ faq }: Props) {
 
+  const animationDuration = 500;
   const ref = useRef<HTMLDivElement>(null);
   const [selected, setSelected] = useState(false)
+  const [maxHeight, setMaxHeight] = useState(0)
 
   useEffect(() => {
     const hash = window.location.hash;
     setSelected(hash === `#${faq.id}`);
+
+    const maxHeight = Array.from(document.querySelectorAll(`[id^="answer-"]`)).sort((a, b) => a.scrollHeight > b.scrollHeight ? -1 : 1)[0].scrollHeight;
+    setMaxHeight(maxHeight);
+
   }, [faq.id]);
 
   return (
@@ -32,9 +38,11 @@ export default function FaqItem({ faq }: Props) {
         </h3>
       </a>
       <div
+        id={`answer-${faq.id}`}
         className={cn(s.answer, selected && s.show, "light")}
         style={{
           maxHeight: selected ? `${ref.current?.scrollHeight}px` : '0px',
+          transitionDuration: `${(animationDuration / maxHeight) * (ref.current?.scrollHeight ?? maxHeight)}ms`
         }}
         ref={ref}
       >
