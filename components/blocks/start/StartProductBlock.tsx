e65dba757ccf3b4f8858eaaae8c90446be69143a
konst-teknik <information@konst-teknik.se>
2023-12-06 12:34:06 +0100

    Fix

--- components/blocks/start/StartProductBlock.tsx
+++ components/blocks/start/StartProductBlock.tsx
@@ -21,7 +21,7 @@ export default async function StartProductBlock({ data: { id, headline, selected
         <h3>{headline}</h3>
         {collection &&
           <Link href={`/shop/${collection?.slug}`}>
-            <span className="body">View all</span>
+            <span className="big">View all</span>
           </Link>
         }
       </header>
