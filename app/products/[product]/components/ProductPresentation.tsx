3340b7158c6170b5d7fe8ce6ef6621ba6ba18a75
Bebe Jane <bjornthief@gmail.com>
2024-01-19 12:34:50 +0100

    product gallery

--- app/products/[product]/components/ProductPresentation.tsx
+++ app/products/[product]/components/ProductPresentation.tsx
@@ -7,6 +7,7 @@ import { StructuredContent } from 'next-dato-utils';
 import { Image } from 'react-datocms';
 import useQueryString from '@lib/hooks/useQueryString';
 import { parseGid } from '@shopify/utils';
+import ProductGallery from './ProductGallery';
 
 export type VariantFormProps = {
   product: ProductQuery['product']
@@ -17,44 +18,55 @@ export type VariantFormProps = {
 export default function ProductPresentation({ product, shopifyProduct }: VariantFormProps) {
 
   const { searchParams } = useQueryString()
-  const [metaSectionToggles, setMetaSectionToggles] = useState<{ [key: string]: boolean }>({})
+  const [galleryId, setGalleryId] = useState<number | null>(null)
+
   const variantId = searchParams.get('variant') ?? null
   const variant = shopifyProduct?.variants.edges.find(({ node }) => parseGid(node.id) === variantId)?.node as ProductVariant ?? shopifyProduct?.variants.edges[0].node as ProductVariant
   const color = variant?.selectedOptions.find(opt => opt.name === 'Color')?.value ?? null
+  const images = product?.sections.map(({ productMedia }) => productMedia.map(({ variation }) => variation).flat().filter(v => v.color?.title?.toLowerCase() === color?.toLowerCase())).flat().filter(v => v.media).map(({ media }) => media).flat()
 
   return (
-    <div className={s.presentation}>
-      {product?.sections.map(({ id, productMedia, text }, i) => {
-        return (
-          <div className={s.section} key={id}>
-            {productMedia.map(({ id, variation, altText: alt }) => {
+    <>
+      <div className={s.presentation}>
+        {product?.sections.map(({ id, productMedia, text }, i) => {
+          return (
+            <div className={s.section} key={id}>
+              {productMedia.map(({ id, variation, altText: alt }) => {
 
-              const mediaCount = productMedia.map(({ variation }) => variation).flat().filter(v => v.color?.title?.toLowerCase() === color?.toLowerCase()).length
-              const selectedVariation = variation.filter(v => v.color?.title?.toLowerCase() === color?.toLowerCase())
-              const media = selectedVariation.map(({ media }) => ({ media })).flat()
+                const mediaCount = productMedia.map(({ variation }) => variation).flat().filter(v => v.color?.title?.toLowerCase() === color?.toLowerCase()).length
+                const selectedVariation = variation.filter(v => v.color?.title?.toLowerCase() === color?.toLowerCase())
+                const media = selectedVariation.map(({ media }) => ({ media })).flat()
 
-              if (mediaCount === 0)
-                return <figure key={id}>No images in color {color}</figure>
+                if (mediaCount === 0)
+                  return <figure key={id}>No images in color {color}</figure>
 
-              return media.map(({ media: { id, responsiveImage } },) =>
-                <figure className={cn(mediaCount > 1 && s.double)} key={id}>
-                  {responsiveImage &&
-                    <Image
-                      key={id}
-                      className={s.image}
-                      pictureClassName={s.picture}
-                      data={{ ...responsiveImage, alt }}
-                    />
-                  }
-                </figure>
-              )
-            })}
-            <div className="big structured">
-              <StructuredContent content={text} />
+                return media.map(({ media: { id, responsiveImage } },) =>
+                  <figure className={cn(mediaCount > 1 && s.double)} key={id} onClick={() => setGalleryId(i)}>
+                    {responsiveImage &&
+                      <Image
+                        key={id}
+                        className={s.image}
+                        pictureClassName={s.picture}
+                        data={{ ...responsiveImage, alt }}
+
+                      />
+                    }
+                  </figure>
+                )
+              })}
+              <div className="big structured">
+                <StructuredContent content={text} />
+              </div>
             </div>
-          </div>
-        )
-      })}
-    </div>
+          )
+        })}
+      </div>
+      <ProductGallery
+        images={images as FileField[]}
+        show={galleryId !== null}
+        index={galleryId}
+        onClose={() => setGalleryId(null)}
+      />
+    </>
   )
 }
