9ab25a245fb0126d4e7e294274774749c155d68d
Bebe Jane <bjornthief@gmail.com>
2024-01-19 22:20:09 +0100

    and

--- app/shop/[collection]/page.tsx
+++ app/shop/[collection]/page.tsx
@@ -1,8 +1,13 @@
 import { CountryShopParams } from '@app/[country]/shop/page';
 import storePage from '../page'
-
 import { AllCollectionsDocument } from '@graphql';
 import { apiQuery } from 'next-dato-utils';
+
+export const runtime = 'edge'
+
+export default async function Collection(params: CountryShopParams) {
+  return storePage(params)
+}
 
 export async function generateStaticParams() {
   const { allCollections } = await apiQuery<AllCollectionsQuery, AllCollectionsQueryVariables>(AllCollectionsDocument, {
@@ -11,8 +16,4 @@ export async function generateStaticParams() {
     tags: ['collection']
   })
   return allCollections.map(({ slug: collection }) => ({ collection }))
-}
-
-export default async function Collection(params: CountryShopParams) {
-  return storePage(params)
 }
