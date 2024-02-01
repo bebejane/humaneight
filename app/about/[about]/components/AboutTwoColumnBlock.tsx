0c61cfd18bdfb7106dfc22172e0911a5c22f4035
Bebe Jane <bjornthief@gmail.com>
2024-02-01 15:16:10 +0100

    fix

--- app/about/[about]/components/AboutTwoColumnBlock.tsx
+++ app/about/[about]/components/AboutTwoColumnBlock.tsx
@@ -18,7 +18,6 @@ export default function AboutTwoColumnBlock({ data: { id, media, text } }: Props
       </figure>
       <div className={cn(s.content, "structured")}>
         <Content content={text} styles={{ 'page-title': s.pageTitle, 'small-text': s.smallText, 'two-columns': s.twoColumns }} />
-        <Content content={text} />
       </div>
     </section>
   )
