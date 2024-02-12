7832d74356c9707e7e439380e4e302ae36d65d0f
konst-teknik <information@konst-teknik.se>
2024-02-12 08:24:29 +0100

    Fix

--- app/products/[product]/components/Feedback.tsx
+++ app/products/[product]/components/Feedback.tsx
@@ -8,9 +8,30 @@ export type Props = {
 export default async function Feedback({ }: Props) {
 
   return (
-    <section className={s.feedback}>
-      <h3>Can we make it better? Something you don’t understand?</h3>
-      <button>Contact us and let us know!</button>
-    </section>
+    <section className={cn(s.feedback, "grid")}>
+      <h3 className="nav">Share your unique knowledge and experiences with us.
+      </h3>
+      <div className={s.wrapper}>
+        <p>Your voice is essential in our journey towards creating a neurodiverse world. Please share your experiences, thoughts, and ideas. Your input will be the backbone for future explorations and things to come.</p>
+        <button>Submit your view</button>
+
+        <form>
+          <p className="mid">Experiences with Our Garments</p><p className="light mid">
+            Please describe your experience with the garments you purchased from us. What did you like or dislike about them? How did they meet your needs or fall short?
+          </p>
+          <input></input>
+
+          <p className="mid"> Living as a Neurodivergent Individual</p><p className="light mid">
+            Please describe the challenges you might face as a neurodivergent individual in todays world. What aspects of neurotypical daily life do you find most challenging, what can be improved?
+          </p>
+          <input></input>
+
+          <p className="mid">Experiences with Our Garments</p><p className="light mid">
+            Please describe your experience with the garments you purchased from us. What did you like or dislike about them? How did they meet your needs or fall short?
+          </p>
+          <input></input>
+        </form>
+      </div >
+    </section >
   )
 }
