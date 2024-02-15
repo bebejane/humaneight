645456982224fe291ffa1a0bc16a212936b96215
Bebe Jane <bjornthief@gmail.com>
2024-02-15 15:40:42 +0100

    size guide

---
+++ components/content/blocks/SizeGuideBlock.tsx
@@ -0,0 +1,42 @@
+import s from './SizeGuideBlock.module.scss'
+import cn from 'classnames'
+import { Image } from 'react-datocms'
+
+type Props = {
+  data: SizeGuideBlockRecord
+  className?: string
+}
+
+type SizeTable = {
+  columns: string[]
+  data: { [key: string]: string }[]
+}
+
+export default function SizeGuideBlock({ data: { id, image, table }, className }: Props) {
+
+  const tableData: SizeTable = table
+  console.log(image)
+  return (
+    <div className={s.sizeGuide}>
+      {image &&
+        <figure>
+          <img src={image.url} className={s.image} />
+        </figure>
+      }
+      {tableData &&
+        <table>
+          <tbody>
+            <tr>
+              {tableData.columns.map((column, idx) => <th key={idx}>{column}</th>)}
+            </tr>
+            {tableData.data.map((row, idx) =>
+              <tr key={idx}>
+                {tableData.columns.map((column, idx) => <td key={idx}>{row[column]}</td>)}
+              </tr>
+            )}
+          </tbody>
+        </table>
+      }
+    </div>
+  )
+}
--- components/content/blocks/index.tsx
+++ components/content/blocks/index.tsx
@@ -1,3 +1,4 @@
 export { default as ContactFormBlock } from './ContactFormBlock'
 export { default as FullscreenBlock } from './FullscreenBlock'
 export { default as TwoColumnBlock } from './TwoColumnBlock'
+export { default as SizeGuideBlock } from './SizeGuideBlock'
