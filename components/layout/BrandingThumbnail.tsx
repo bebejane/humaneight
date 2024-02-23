db0d5460f0395670791702c8578bcc863b414468
Bebe Jane <bjornthief@gmail.com>
2024-02-23 12:04:01 +0100

    fix branding link

--- components/layout/BrandingThumbnail.tsx
+++ components/layout/BrandingThumbnail.tsx
@@ -7,23 +7,25 @@ export type Props = {
   productBranding: ProductBrandingRecord,
   columns?: string | undefined
 }
-export default function BrandingThumbnail({ productBranding: { text, smallText, image, }, columns = 'three' }: Props) {
+export default function BrandingThumbnail({ productBranding: { text, smallText, image, link }, columns = 'three' }: Props) {
 
 
   return (
-    <figure className={cn(s.thumbnail, s[columns])}>
-      {image?.responsiveImage &&
-        <Image
-          data={image.responsiveImage}
-          className={cn(s.image)}
-          placeholderClassName={s.picture}
-          pictureClassName={s.picture}
-        />
-      }
-      <h3 className="nav">{text}</h3>
-      <figcaption>
-        <p className="mid-small">{smallText}</p>
-      </figcaption>
-    </figure >
+    <Link className={s.thumbnail} href={`/about/${link?.slug}`}>
+      <figure className={cn(s[columns])}>
+        {image?.responsiveImage &&
+          <Image
+            data={image.responsiveImage}
+            className={cn(s.image)}
+            placeholderClassName={s.picture}
+            pictureClassName={s.picture}
+          />
+        }
+        <h3 className="nav">{text}</h3>
+        <figcaption>
+          <p className="mid-small">{smallText}</p>
+        </figcaption>
+      </figure >
+    </Link>
   )
 }
