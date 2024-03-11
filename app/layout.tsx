a67d0b863678ed4d41865a291a4b5f4c79c5e579
Bebe Jane <bjornthief@gmail.com>
2024-03-11 18:26:57 +0100

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,8 @@ import Script from 'next/script';
 import NavBar from '@components/nav/NavBar';
 import Footer from '@components/nav/Footer';
 import CookieConsent from '@components/common/CookieConsent';
-import { GoogleTagManager } from '@next/third-parties/google'
+import { GoogleAnalytics } from '@next/third-parties/google'
+
 import { apiQuery } from 'next-dato-utils/api';
 import { AllProductsDocument, GlobalDocument, GeneralDocument } from '@graphql';
 import { Metadata } from 'next';
@@ -57,7 +58,7 @@ export default async function RootLayout({ children }: LayoutProps) {
           />
           <CookieConsent />
           <Script src="https://ac.onthemapmarketing.com/widget/3b24ef8d-6704-4361-886d-da817089839e/autoload.js" />
-          <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID} />
+          <GoogleAnalytics gaId={process.env.GOOGLE_ANALYTICS_ID} />
         </body>
       </html >
     </>
