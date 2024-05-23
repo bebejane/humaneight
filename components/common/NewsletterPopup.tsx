cf47f0bf6d8f1841a31ed6b979045cf0c21d4c55
Bebe Jane <bjornthief@gmail.com>
2024-05-23 16:21:59 +0200

    fixes

--- components/common/NewsletterPopup.tsx
+++ components/common/NewsletterPopup.tsx
@@ -17,12 +17,13 @@ export default function NewsletterPopup({ show: _show, onClose }: Props) {
   const autoShowRef = useRef<NodeJS.Timeout>()
 
   const handleClose = () => {
+    localStorage.setItem('newsletter_closed', '1')
     setShow(false)
     onClose?.()
   }
 
   useEffect(() => {
-    if (localStorage.getItem('newsletter_subscribed'))
+    if (localStorage.getItem('newsletter_subscribed') || localStorage.getItem('newsletter_closed'))
       return setShow(false)
 
     autoShowRef.current = setTimeout(() => setShow(true), 15000)
