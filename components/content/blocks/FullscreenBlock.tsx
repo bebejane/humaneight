5ce1376a3565755aec75ed375b67e0c198dc26e4
Bebe Jane <bjornthief@gmail.com>
2024-05-27 15:39:36 +0200

    fiiiixeeeesssss ahhhhh

--- components/content/blocks/FullscreenBlock.tsx
+++ components/content/blocks/FullscreenBlock.tsx
@@ -1,6 +1,10 @@
+'use client'
+
 import s from './FullscreenBlock.module.scss'
 import cn from 'classnames'
 import { Image } from 'react-datocms/image'
+import { useInterval } from 'react-use'
+import { useState } from 'react'
 
 type Props = {
   data: FullscreenBlockRecord & { mediaMobile?: FileField, altMediaMobile?: FileField }
@@ -12,6 +16,11 @@ export default function FullscreenBlock({ data: { id, media, altMedia, mediaMobi
   const medias = [media, altMedia].filter(el => el)
   const mediasMobile = [mediaMobile, altMediaMobile].filter(el => el)
   const columns = medias.length > 1 ? 'two' : 'one'
+  const [mobileIndex, setMobileIndex] = useState(0)
+
+  useInterval(() => {
+    setMobileIndex((mobileIndex + 1) === mediasMobile.length ? 0 : mobileIndex + 1)
+  }, 3000)
 
   return (
     <div className={cn(s.container, className, s[columns])}>
@@ -40,7 +49,7 @@ export default function FullscreenBlock({ data: { id, media, altMedia, mediaMobi
       )}
       {mediasMobile.map((m, i) =>
         m?.responsiveImage ?
-          <figure key={i} className={s.mobile}>
+          <figure key={i} className={cn(s.mobile, mobileIndex === i && s.visible)}>
             <Image
               data={m.responsiveImage}
               className={s.image}
@@ -56,11 +65,15 @@ export default function FullscreenBlock({ data: { id, media, altMedia, mediaMobi
             muted
             loop
             playsInline
-            className={s.mobile}
+            className={cn(s.mobile, mobileIndex === i && s.visible)}
             poster={`${m?.video?.thumbnailUrl}?time=0`}
             src={m?.video?.mp4Url}
           />
       )}
     </div>
   )
+}
+
+function setState(arg0: number) {
+  throw new Error('Function not implemented.')
 }
