bcb0d710acc150740a58f89c7bafdebd464ac5c9
Bebe Jane <bjornthief@gmail.com>
2025-10-07 14:23:34 +0200

    fix broken cart

--- components/common/LottieAnimation.tsx
+++ components/common/LottieAnimation.tsx
@@ -1,23 +1,24 @@
-'use client'
+'use client';
 
-import Lottie from "lottie-react";
-import { useAsync } from "react-use";
+import { lazy } from 'react';
+import { useAsync } from 'react-use';
+
+const Lottie = lazy(() => import('lottie-react'));
 
 export type Props = {
-  url: string
-  className?: string
-}
+	url: string;
+	className?: string;
+};
 
 export default function LottieAnimation({ url, className }: Props) {
+	const { value, error } = useAsync(async () => {
+		const data = await fetch(url).then((res) => res.json());
+		return data;
+	}, [url]);
 
-  const { value, error } = useAsync(async () => {
-    const data = await fetch(url).then((res) => res.json())
-    return data;
-  }, [url])
+	error && console.error(error);
 
-  error && console.error(error)
+	if (!value) return null;
 
-  if (!value) return null
-
-  return <Lottie animationData={value} className={className} />
+	return <Lottie animationData={value} className={className} />;
 }
