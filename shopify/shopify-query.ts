2e44e564dac587b0b2ba8a419ad66e045a619f0b
Bebe Jane <bjornthief@gmail.com>
2025-10-12 21:41:42 +0200

    fix shopify query errors

--- shopify/shopify-query.ts
+++ shopify/shopify-query.ts
@@ -1,109 +1,105 @@
-
-import type { RequestInit } from 'next/dist/server/web/spec-extension/request'
-import type { DocumentNode } from 'graphql'
-import { print } from 'graphql/language/printer'
+import type { RequestInit } from 'next/dist/server/web/spec-extension/request';
+import type { DocumentNode } from 'graphql';
+import { print } from 'graphql/language/printer';
 import isInteger from 'is-integer';
 
 const shopifyApiEndpoint = `https://${process.env.NEXT_PUBLIC_SHOPIFY_STORE}.myshopify.com/api/${process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_API_VERSION}/graphql.json`;
 
 export type ApiQueryOptions<V = void> = {
-  variables?: V;
-  revalidate?: number | undefined;
-  tags?: string[] | undefined,
-  logs?: boolean
-  all?: boolean,
-  country?: string
+	variables?: V;
+	revalidate?: number | undefined;
+	tags?: string[] | undefined;
+	logs?: boolean;
+	all?: boolean;
+	country?: string;
 };
 
 export type DefaultApiQueryOptions = ApiQueryOptions & {
-  variables: undefined,
-  revalidate: number | undefined,
-  tags: string[] | undefined,
-  logs: boolean
-  all: boolean
-}
-
-const defaultOptions: DefaultApiQueryOptions = {
-  variables: undefined,
-  revalidate: 0,
-  tags: undefined,
-  logs: false,
-  all: false
+	variables: undefined;
+	revalidate: number | undefined;
+	tags: string[] | undefined;
+	logs: boolean;
+	all: boolean;
 };
 
+const defaultOptions: DefaultApiQueryOptions = {
+	variables: undefined,
+	revalidate: 0,
+	tags: undefined,
+	logs: false,
+	all: false,
+};
 
-export default async function shopifyQuery<T = void, V = void>(query: DocumentNode, options?: ApiQueryOptions<V>): Promise<T> {
+export default async function shopifyQuery<T = void, V = void>(
+	query: DocumentNode,
+	options?: ApiQueryOptions<V>
+): Promise<T> {
+	const opt = { ...defaultOptions, ...(options ?? {}) };
 
-  const opt = { ...defaultOptions, ...(options ?? {}) };
+	if (!process.env.NEXT_PUBLIC_SHOPIFY_STORE) throw new Error('NEXT_PUBLIC_SHOPIFY_STORE is not set');
+	if (!process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_API_VERSION)
+		throw new Error('NEXT_PUBLIC_SHOPIFY_STOREFRONT_API_VERSION is not set');
+	if (!process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_API_TOKEN)
+		throw new Error('NEXT_PUBLIC_SHOPIFY_STOREFRONT_API_TOKEN is not set');
 
-  if (!process.env.NEXT_PUBLIC_SHOPIFY_STORE)
-    throw new Error('NEXT_PUBLIC_SHOPIFY_STORE is not set')
-  if (!process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_API_VERSION)
-    throw new Error('NEXT_PUBLIC_SHOPIFY_STOREFRONT_API_VERSION is not set')
-  if (!process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_API_TOKEN)
-    throw new Error('NEXT_PUBLIC_SHOPIFY_STOREFRONT_API_TOKEN is not set')
+	const queryId = (query.definitions?.[0] as any).name?.value as string;
+	const country = ((opt.country as CountryCode) ?? 'SE').toUpperCase();
 
-  const queryId = (query.definitions?.[0] as any).name?.value as string
-  const country = (opt.country as CountryCode ?? 'SE').toUpperCase();
+	const dedupeOptions: DedupeOptions = {
+		body: JSON.stringify({
+			query: print(query),
+			variables: options?.variables ? { ...options.variables, country } : { country },
+		}) as string,
+		...opt,
+		queryId,
+	};
 
-  const dedupeOptions: DedupeOptions = {
-    body: JSON.stringify({
-      query: print(query),
-      variables: options?.variables ? { ...options.variables, country } : { country }
-    }) as string,
-    ...opt,
-    queryId
-  }
-
-  const { data } = await dedupedFetch({ ...dedupeOptions, tags: [] });
-  return { ...data }
+	const { data } = await dedupedFetch({ ...dedupeOptions, tags: [] });
+	return { ...data };
 }
 
 export type DedupeOptions = {
-  url?: string
-  body: string;
-  revalidate?: number;
-  tags?: string[] | undefined
-  queryId: string,
-  logs: boolean
-}
+	url?: string;
+	body: string;
+	revalidate?: number;
+	tags?: string[] | undefined;
+	queryId: string;
+	logs: boolean;
+};
 
 const dedupedFetch = async (options: DedupeOptions) => {
-  const {
-    url,
-    body,
-    revalidate,
-    tags,
-    queryId,
-    logs
-  } = options;
+	const { url, body, revalidate, tags, queryId, logs } = options;
 
-  const headers = {
-    'X-Shopify-Storefront-Access-Token': (process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_API_TOKEN || process.env.SHOPIFY_STOREFRONT_API_TOKEN) as string,
-    'Content-Type': 'application/json'
-  } as unknown as HeadersInit
+	const headers = {
+		'X-Shopify-Storefront-Access-Token': (process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_API_TOKEN ||
+			process.env.SHOPIFY_STOREFRONT_API_TOKEN) as string,
+		'Content-Type': 'application/json',
+	} as unknown as HeadersInit;
 
-  const response = await fetch(shopifyApiEndpoint, {
-    method: 'POST',
-    headers,
-    body,
-    next: {
-      revalidate,
-      cache: 'no-store',
-      tags: Array.isArray(tags) ? tags : undefined
-    }
-  } as RequestInit);
+	const response = await fetch(shopifyApiEndpoint, {
+		method: 'POST',
+		headers,
+		body,
+		next: {
+			revalidate,
+			cache: 'no-store',
+			tags: Array.isArray(tags) ? tags : undefined,
+		},
+	} as RequestInit);
 
-  const responseBody = await response.json();
+	const responseBody = await response.json();
 
-  if (!response.ok)
-    throw new Error(`${response.status} ${response.statusText}: ${JSON.stringify(responseBody)}`);
+	if (!response.ok) {
+		console.error(JSON.stringify(response, null, 2));
+		console.error(`${response.status} ${response.statusText}`);
+		throw new Error(`${response.status} ${response.statusText}`);
+	}
 
-  if (responseBody.errors) {
-    console.error(responseBody.errors)
-    const message = responseBody.errors.map(({ message }: { message: string }) => message).join('. ')
-    throw new Error(message);
-  }
-  logs && console.log(queryId, { ...options, body: undefined }, response.headers.get('x-cache'))
-  return responseBody;
-}
+	if (responseBody.errors) {
+		console.error(responseBody.errors);
+		const message = responseBody.errors.map(({ message }: { message: string }) => message).join('. ');
+		throw new Error(message);
+	}
+	logs && console.log(queryId, { ...options, body: undefined }, response.headers.get('x-cache'));
+	return responseBody;
+};
