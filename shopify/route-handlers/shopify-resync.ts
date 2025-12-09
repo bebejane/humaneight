50eb4fe5e1e5bd6eb60405be0c3cca6d694030d4
Bebe Jane <bjornthief@gmail.com>
2025-12-09 19:24:16 +0100

    j

--- shopify/route-handlers/shopify-resync.ts
+++ shopify/route-handlers/shopify-resync.ts
@@ -3,20 +3,17 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { syncAll } from '../sync';
 import { parseDatoCMSApiError } from 'next-dato-utils/utils';
-import { basicAuth } from 'next-dato-utils/route-handlers';
 
-export default async function resync(request: NextRequest) {
-	return basicAuth(request, async (req) => {
-		try {
-			const now = Date.now();
-			await syncAll();
-			return NextResponse.json({ success: true, time: Date.now() - now });
-		} catch (error) {
-			//console.log(JSON.stringify(error))
-			console.log(error);
-			console.log(parseDatoCMSApiError(error));
+export default async function resync(req: NextRequest) {
+	try {
+		const now = Date.now();
+		await syncAll();
+		return NextResponse.json({ success: true, time: Date.now() - now });
+	} catch (error) {
+		//console.log(JSON.stringify(error))
+		console.log(error);
+		console.log(parseDatoCMSApiError(error));
 
-			return NextResponse.json({ success: false, error: parseDatoCMSApiError(error) });
-		}
-	});
+		return NextResponse.json({ success: false, error: parseDatoCMSApiError(error) });
+	}
 }
--- shopify/route-handlers/shopify-sync.ts
+++ shopify/route-handlers/shopify-sync.ts
@@ -1,29 +1,26 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { syncObjects, deleteObject } from '../sync';
-import { basicAuth } from 'next-dato-utils/route-handlers';
 
-export default async function shopifyToDatoCMSSync(request: NextRequest) {
-	return basicAuth(request, async (req) => {
-		try {
-			const event = (req.headers.get('x-shopify-topic') as string)?.split('/')?.[1];
+export default async function shopifyToDatoCMSSync(req: NextRequest) {
+	try {
+		const event = (req.headers.get('x-shopify-topic') as string)?.split('/')?.[1];
 
-			if (!event)
-				return NextResponse.json({ success: false, error: 'event type not found in headers' });
+		if (!event)
+			return NextResponse.json({ success: false, error: 'event type not found in headers' });
 
-			const data: any = await req.json();
+		const data: any = await req.json();
 
-			if (!data) return NextResponse.json({ success: false, error: 'item not found' });
+		if (!data) return NextResponse.json({ success: false, error: 'item not found' });
 
-			console.log('sync', event, data.id, data.title);
+		console.log('sync', event, data.id, data.title);
 
-			if (['create', 'update'].includes(event)) await syncObjects(data);
-			//else if (event === 'delete')
-			//await deleteObject(data)
+		if (['create', 'update'].includes(event)) await syncObjects(data);
+		//else if (event === 'delete')
+		//await deleteObject(data)
 
-			return NextResponse.json({ success: true, event });
-		} catch (error) {
-			console.log(JSON.stringify(error, null, 2));
-			return NextResponse.json({ success: false, error: (error as Error).message });
-		}
-	});
+		return NextResponse.json({ success: true, event });
+	} catch (error) {
+		console.log(JSON.stringify(error, null, 2));
+		return NextResponse.json({ success: false, error: (error as Error).message });
+	}
 }
