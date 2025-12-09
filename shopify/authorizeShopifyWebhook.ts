1c4e2941966dbfe31662180ca64dca938995778c
Bebe Jane <bjornthief@gmail.com>
2025-12-09 20:48:33 +0100

    fixed webhook

--- shopify/authorizeShopifyWebhook.ts
+++ shopify/authorizeShopifyWebhook.ts
@@ -4,11 +4,9 @@ export default async function authorizeShopifyWebhook(
 	req: Request,
 	callback?: (body: any) => Promise<Response>
 ): Promise<Response> {
-	console.log('test auth webhook');
 	const body = await req.text();
 	const event = (req.headers.get('x-shopify-topic') as string)?.split('/')?.[1];
 	if (!event) {
-		console.log('no event');
 		return Response.json(
 			{ success: false, error: 'event type not found in headers' },
 			{ status: 401 }
@@ -21,11 +19,7 @@ export default async function authorizeShopifyWebhook(
 		.update(body, 'utf8')
 		.digest('base64');
 
-	console.log(digest, 'digest');
-	console.log(hmacHeader, 'hmac');
 	const verified = crypto.timingSafeEqual(Buffer.from(digest), Buffer.from(hmacHeader));
-	console.log('verified', verified);
-	console.log('body', body);
 	if (verified) {
 		return await callback(JSON.parse(body));
 	} else {
