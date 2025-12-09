f60c5b69daa5bedf7484d50c29821679d1c2af75
Bebe Jane <bjornthief@gmail.com>
2025-12-09 20:21:58 +0100

    t

--- app/(shopify)/api/shopify-resync/route.ts
+++
@@ -1,3 +0,0 @@
-export { default as GET } from '@/shopify/route-handlers/shopify-resync';
-export const runtime = 'nodejs';
-export const dynamic = 'force-dynamic';
---
+++ shopify/authorizeShopifyWebhook.ts
@@ -0,0 +1,36 @@
+import { NextRequest } from 'next/server';
+import * as crypto from 'crypto';
+
+export default async function authorizeShopifyWebhook(
+	req: Request,
+	callback?: (body: any) => Promise<Response>
+): Promise<Response> {
+	console.log('test auth webhook');
+	const body = await req.json();
+	const event = (req.headers.get('x-shopify-topic') as string)?.split('/')?.[1];
+	if (!event) {
+		console.log('no event');
+		return Response.json(
+			{ success: false, error: 'event type not found in headers' },
+			{ status: 401 }
+		);
+	}
+
+	const hmacHeader = req.headers.get('X-Shopify-Hmac-Sha256');
+	const digest = crypto
+		.createHmac('sha256', process.env.SHOPIFY_WEBHOOK_VERIFY_INTEGRITY)
+		.update(JSON.stringify(body), 'utf8')
+		.digest('base64');
+
+	console.log(digest, 'digest');
+	const verified = crypto.timingSafeEqual(Buffer.from(digest), Buffer.from(hmacHeader));
+	console.log('verified', verified);
+	console.log('body', body);
+	if (verified) {
+		return await callback(body);
+	} else {
+		return new Response('Unauthorized Request', {
+			status: 401,
+		});
+	}
+}
