312e48af9de1c7df6f01071f460b239c2a5b6b14
Bebe Jane <bjornthief@gmail.com>
2026-06-17 12:07:22 +0200

    j

---
+++ app/api/withdraw-from-purchase/route.ts
@@ -0,0 +1,43 @@
+import { sendWithdrawFromPurchaseEmail, sendWithdrawFromPurchaseReplyEmail } from '@/lib/email';
+import { WithdrawFromPurchaseFormSchema } from '@/app/withdraw-from-purchase/schema';
+import { ZodError } from 'zod';
+import { isbot } from 'isbot';
+
+export async function POST(req: Request) {
+	try {
+		const data = await req.json();
+		const { order_number, email, confirm_email, message } =
+			WithdrawFromPurchaseFormSchema.parse(data);
+
+		if (isbot(req.headers.get('User-Agent')) || (confirm_email && confirm_email?.length > 0)) {
+			console.log('cancel-purchase form', 'bot detected');
+			return new Response(JSON.stringify({ success: false, error: 'Bots are not allowed.' }), {
+				status: 403,
+			});
+		}
+
+		await sendWithdrawFromPurchaseEmail({
+			email: process.env.POSTMARK_FROM_EMAIL as string,
+			orderNo: order_number,
+			message,
+		});
+
+		await sendWithdrawFromPurchaseReplyEmail({
+			email,
+			orderNo: order_number,
+		});
+
+		return new Response(JSON.stringify({ success: true }), { status: 200 });
+	} catch (e) {
+		console.log(e);
+		if (e instanceof ZodError) {
+			return new Response(JSON.stringify({ invalid: e, success: false }), { status: 200 });
+		} else
+			return new Response(
+				JSON.stringify({ success: false, error: typeof e === 'string' ? e : (e as Error).message }),
+				{
+					status: 500,
+				},
+			);
+	}
+}
---
+++ app/withdraw-from-purchase/WithdrawFromPurchaseForm.tsx
@@ -0,0 +1,129 @@
+'use client';
+
+import s from './WithdrawFromPurchaseForm.module.scss';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import Loader from '@/components/common/Loader';
+import z, { ZodError } from 'zod';
+import { useState } from 'react';
+import { WithdrawFromPurchaseFormSchema } from './schema';
+
+export type Props = {
+	eMailText: string;
+};
+
+type WithdrawFromPurchaseFormInputs = z.infer<typeof WithdrawFromPurchaseFormSchema>;
+
+const initialState: any = {
+	order_number: '',
+	email: '',
+	confirm_email: '',
+	message: '',
+};
+
+export default function WithdrawFromPurchaseForm({ eMailText }: Props) {
+	const { register, handleSubmit } = useForm<WithdrawFromPurchaseFormInputs>();
+	const [loading, setLoading] = useState(false);
+	const [success, setSuccess] = useState(false);
+	const [error, setError] = useState<string | null>(null);
+	const [invalid, setInvalid] = useState<ZodError<WithdrawFromPurchaseFormInputs> | null>(null);
+
+	const onSubmit: SubmitHandler<WithdrawFromPurchaseFormInputs> = async (data) => {
+		try {
+			setLoading(true);
+			setSuccess(false);
+			setError(null);
+			setInvalid(null);
+
+			const body = WithdrawFromPurchaseFormSchema.parse(data);
+			const res = await fetch('/api/withdraw-from-purchase', {
+				method: 'POST',
+				body: JSON.stringify(body),
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			});
+
+			const json = await res.json();
+
+			if (res.ok) {
+				if (json.invalid) setInvalid(json.invalid as ZodError);
+				setSuccess(json.success);
+			} else {
+				setError(json.error?.message ?? json.error ?? 'Something went wrong');
+			}
+		} catch (e) {
+			if (e instanceof ZodError) setInvalid(e);
+			else setError(typeof e === 'string' ? e : (e as Error).message);
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	const errors = (id: string) => {
+		const message = invalid?.issues?.find((e) => e?.path?.includes(id))?.message;
+		return message ? <div className={s.error}>{message}</div> : null;
+	};
+
+	return (
+		<div className={s.container}>
+			<form id='cancel-purchase-form' onSubmit={handleSubmit(onSubmit)} className={s.form}>
+				<div className={s.wrap}>
+					<label htmlFor='order_number' className='medium'>
+						Order No.
+					</label>
+					<input
+						id='order_number'
+						type='text'
+						autoComplete='off'
+						defaultValue={initialState.order_number}
+						{...register('order_number')}
+					/>
+					{errors('order_number')}
+				</div>
+				<div className={s.wrap}>
+					<label htmlFor='email' className='medium'>
+						E-mail
+					</label>
+					<input id='email' type='email' defaultValue={initialState.email} {...register('email')} />
+					{errors('email')}
+					<input
+						id='confirm_email'
+						type='text'
+						defaultValue={initialState.confirm_email}
+						{...register('confirm_email')}
+						style={{ display: 'none' }}
+					/>
+				</div>
+				<div className={s.wrap}>
+					<label htmlFor='message' className='medium'>
+						Message
+					</label>
+					<textarea
+						className='medium'
+						id='message'
+						defaultValue={initialState.message}
+						{...register('message')}
+					/>
+					{errors('message')}
+				</div>
+				<button type='submit'>Send cancellation request</button>
+			</form>
+			{error && (
+				<div className={s.formerror}>
+					<h2>Something went wrong</h2>
+					<span>{error}</span>
+				</div>
+			)}
+			{loading && (
+				<div className={s.loading}>
+					<Loader invert={true} />
+				</div>
+			)}
+			{success && (
+				<div className={s.success}>
+					<h1>Thank you. A confirmation has been sent to your email address.</h1>
+				</div>
+			)}
+		</div>
+	);
+}
